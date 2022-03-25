7092cce98a628826f202e544dc3313287d8a1b96
jeannetejada <98044855+jeannetejada@users.noreply.github.com>
2022-03-25 08:40:59 -0700

    Update algorithm.js

--- hw2/algorithm.js
+++ hw2/algorithm.js
@@ -1,1 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
 
+<h2>JavaScript Array of Integers</h2>
+
+<p id="demo"></p>
+
+<script>
+document.getElementById("demo").innerHTML =
+Math.floor(Math.random() * 1000) + 1;
+</script>
+
+</body>
+</html>
